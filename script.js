8c0e4eb25574a71ba453f9f3c5a1798ea7957b06
tliguimohamed <92030258+tliguimohamed@users.noreply.github.com>
2022-01-06 12:20:56 +0100

    bonjour

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
 
 
 
-    var titre=document.getElementById("titre")
-    var aut=document.getElementById("Auteur")
-    var emal= document.getElementById("email")
-    var prix=document.getElementById("Prix")
-    var date=document.getElementById("date")
-    var lang=document.getElementById("fname")
-    var rad = document.getElementsByClassName ("radio-1")
+    var titre =document.getElementById("titre")
+    var aut =document.getElementById("Auteur")
+    var emal = document.getElementById("email")
+    var prix =document.getElementById("Prix")
+    var date =document.getElementById("date")
+    var lang =document.getElementById("fname")
 
-class ouvrage {
-    constructor(Titre,Auteur,Email ,Prix,date,Langue,Type){
-        this.titre=Titre;
-        this.auteur=Auteur;
-        this.email=Email;
+    var rad = document.getElementsByClassName ("radio-1")
+    var tbody = document.getElementsByTagName("tbody")[0];
+    var liste =[];
+    var list2= ""
+    var from_valide = true;
+class Ouvrage {
+    constructor(titre,auteur,email ,Prix,date,Langs,type){
+        this.titre=titre;
+        this.auteur=auteur;
+        this.email=email;
         this.prix=Prix;
-        this.dates=date;
-        this.Langs=Langue;
-        this.type=Type;
+        this.date=date;
+        this.Langs=Langs;
+        this.type=type;
     
     }
+    
+    DétailOuvrage(){
+        return "L'ouvrage" +this. titre + "est un" + auteur + "en langue" +Langs+ "écrit par" + auteur + "et publié le" + dates + "Le prix de" + titre + "est de" + prix + "Dhs"
+    }
+        
+    }
+    //  localStorage
 
-}
+    list2=JSON.parse(localStorage.getItem("list"));
+    if (list2 != null) {
+        for(i=0;i<list2.length;i++){
+            var livre_info = new Ouvrage(list2[i].titre, list2[i].auteur, list2[i].email, list2[i].prix, list2[i].date, list2[i].Langs, list2[i].type)
+            liste.push(livre_info);
+        }
+
+    }
+  
+
+    
+
 
 
 
@@ -33,8 +55,8 @@ myform.addEventListener("submit",function(e){
 
     
 
-  var from_valide = true
-  var emi = /\S+@gmail\.com/
+    
+    var emi = /\S+@gmail\.com/
   
 
 
@@ -43,49 +65,57 @@ myform.addEventListener("submit",function(e){
         var errortitre=document.getElementById("titre-1")
         errortitre.innerHTML=('Remplir le champs')
         errortitre.style.color="red"
-      }
+        from_valide = false
+    }
     else if(!isNaN(titre.value)){
-      
         var errortitre=document.getElementById("titre-1")
         errortitre.innerHTML=('Only text')
         errortitre.style.color="red"
-    
-    
+        from_valide = false
+
     } 
     else{
         var errortitre=document.getElementById("titre-1")
         errortitre.innerHTML=('Good')
         errortitre.style.color="green"
+        from_valide = true
     }
     if(titre.value.length>20){ 
         var errortitre=document.getElementById("titre-1")
         errortitre.innerHTML=('Titre de louvrage max 20')
         errortitre.style.color="red"
+        from_valide = false
+
       }
     
       if(aut.value== ""){ 
           var errorauteur=document.getElementById("auteur-1")
           errorauteur.innerHTML=('Remplir le champs')
           errorauteur.style.color="red"
+          from_valide = false
+
         }
     
         else if(!isNaN(aut.value)){
-          
             var errorauteur=document.getElementById("auteur-1")
             errorauteur.innerHTML=('Only text')
             errorauteur.style.color="red"
-        
-        
+            from_valide = false
+
         } 
         else{
             var errorauteur=document.getElementById("auteur-1")
             errorauteur.innerHTML=('Good')
             errorauteur.style.color="green"
+            from_valide = true
+
         }
         if(aut.value.length>20){ 
             var errorauteur=document.getElementById("auteur-1")
             errorauteur.innerHTML=('Titre de louvrage max 20')
             errorauteur.style.color="red"
+            from_valide = false
+
           }
         // Email
     var erroremal = document.getElementById("email-1")
@@ -98,27 +128,32 @@ myform.addEventListener("submit",function(e){
     else if(emi.test(emal.value) == false){
         erroremal.innerHTML=('Only text')
         erroremal.style.color="red"
+        from_valide = false;
+
+
       }
       else{
-        erroremal.innerHTML = "good";
+        erroremal.innerHTML = "Good";
         erroremal.style.color ="green"
+        from_valide = true;
+
       }
     
 
           var prix=document.getElementById("Prix")
           if((prix.value).trim() == ""){
-
-    var errorprix=document.getElementById("prix-1")
-    errorprix.innerHTML=('Message no validé')
-    errorprix.style.color="red"
-
+          var errorprix=document.getElementById("prix-1")
+          errorprix.innerHTML=('Message no validé')
+          errorprix.style.color="red"
+          from_valide = false;
 
 }
 else if(isNaN(prix.value)){
-
     var errorprix=document.getElementById("prix-1")
     errorprix.innerHTML=('Message no validé')
     errorprix.style.color="red"
+    from_valide = false;
+
 
 
 }
@@ -126,12 +161,15 @@ else if((prix.value)<=0){
     var errorprix=document.getElementById("prix-1")
     errorprix.innerHTML=('Le prix doit etre positif')
     errorprix.style.color="red"
+    from_valide = false;
+
 }
 
 else{
     var errorprix =document.getElementById("prix-1")
     errorprix.innerHTML=('Good')
     errorprix.style.color="green"
+    from_valide = true;
 
 }
 
@@ -140,80 +178,89 @@ if(date.value== "" ) {
     var errordate=document.getElementById("date-1")
     errordate.innerHTML=('Message no validé')
     errordate.style.color="red"
+    from_valide = false;
+
  }
 else{
         var errordate=document.getElementById("date-1")
         errordate.innerHTML=('Good')
         errordate.style.color="green"
-    
+        from_valide = true;
+
 }
 if(lang.value== "" ) { 
     var errorlang=document.getElementById("lang-1")
     errorlang.innerHTML=('Message no validé')
     errorlang.style.color="red"
+    from_valide = false;
+
  }
     else{
         var errorlang=document.getElementById("lang-1")
         errorlang.innerHTML=('Good')
         errorlang.style.color="green"
+        from_valide = true;
+
  }
 
 
 //radio
-if (!(rad[0].checked || rad[1].checked) || rad[2].checked) {
+if (!(rad[0].checked || rad[1].checked || rad[2].checked)) {
     var errorradio = document.getElementById("rad-1");
-    from_valide = false;
     errorradio.innerHTML=("Veuillez sélectionner un type");
     errorradio.style.color = "red";
+    from_valide = false;
+
 }
 
 else{
     var errorradio = document.getElementById("rad-1")
     errorradio.innerHTML=("Good")
     errorradio.style.color="green"
+    
+
 
 }
-// // Email
-// var erroremal = document.getElementById("email-1")
-
-// if(emal.value== ""){ 
-//     erroremal.innerHTML=('Remplir le champs')
-//     erroremal.style.color="red"
-//     from_valide = false;
-//   }
-// else if(emi.test(emal.value) == false){
-//     erroremal.innerHTML=('Only text')
-//     erroremal.style.color="red"
-//   }
-//   else{
-//     erroremal.innerHTML = "good";
-//     erroremal.style.color ="green"
-//   }
 
 
-                
+
+if (from_valide == true){
+    var type_btn = document.querySelector(".radio-1:checked")
+    var ouvrage =  new Ouvrage(titre.value,aut.value,emal.value,prix.value,date.value,lang.options[lang.selectedIndex].value,type_btn.value)
+                       
+// ajouter l'objet ouvrage dans une liste JS
+liste.push(ouvrage)
+localStorage.setItem("list" , JSON.stringify(liste))
+
+// trier la liste
+trier();
+tbody.innerHTML = "";
+for(var i=0;i<liste.length;i++){
+    var row = tbody.insertRow(-1);
+    row.insertCell(0).innerHTML = liste[i].titre;
+    row.insertCell(1).innerHTML = liste[i].auteur;
+    row.insertCell(2).innerHTML = liste[i].email;                       
+    row.insertCell(3).innerHTML = liste[i].prix;
+    row.insertCell(4).innerHTML = liste[i].date;
+    row.insertCell(5).innerHTML = liste[i].Langs;
+    row.insertCell(6).innerHTML = liste[i].type;
+    row.insertCell(7).innerHTML = 
+'<button onclick= "Edit(this)" value= "Edit" >Edit</button>' + '<button Onclick="deleteRow(this)" value="delet">Delete</button>' ;
+}
+}
+
          
-                       var row = table.insertRow(-1);
-                       var ouvrage=ouvrages(titre,aut,emal,prix,date,rad.value)
-                       row.insertCell(0).innerHTML = titre.value;
-                       row.insertCell(1).innerHTML = Auteur.value;
-                       row.insertCell(2).innerHTML = email.value;                       
-                       row.insertCell(3).innerHTML = Prix.value;
-                       row.insertCell(4).innerHTML = date.value;
-                       row.insertCell(5).innerHTML =  lang.options[lang.selectedIndex].value;
+                       
+
+                        
                        
                      
                      
                      
-    var temp_cell="" ;
-   for(var i=0;i<rad.length;i++){ 
-               if(rad[i].checked){
-                 temp_cell = rad[i].value;
-                 }
-            }
-            row.insertCell(5).innerHTML = temp_cell;
-            row.insertCell(6).innerHTML = 
-            '<button onclick= "Edit(this)" value= "Edit" >Edit</button>' + '<button Onclick="deleteRow(this)" value="delet">Delete</button>' ;
+    
+                        
+                        
+            
            
 } 
 )
@@ -227,16 +274,17 @@ function Edit(r) {
     var rad = document.getElementsByClassName ("radio-1")
     var i = r.parentNode.parentNode.rowIndex;
     var row = table.rows[i];
-    var lang= document.getElementById("fname");
+
 
 
 
     if (r.innerHTML == "Edit") {
         titre.value = row.cells[0].innerHTML;
         Auteur.value = row.cells[1].innerHTML;
-        Prix.value = row.cells[2].innerHTML;
-        date.value = row.cells[3].innerHTML;
-        document.getElementById("fname").value = row.cells[4].innerHTML;
+        email.value = row.cells[2].innerHTML;
+        Prix.value = row.cells[3].innerHTML;
+        date.value = row.cells[4].innerHTML;
+        document.getElementById("fname").value = row.cells[5].innerHTML;
 
         // Type
         for (var i = 0; i < rad.length; i++) {
@@ -250,16 +298,39 @@ function Edit(r) {
     else {
         row.cells[0].innerHTML = titre.value;
         row.cells[1].innerHTML = Auteur.value;
-        row.cells[2].innerHTML = Prix.value;
-        row.cells[3].innerHTML = date.value;
-        row.cells[4].innerHTML = lang.options[lang.selectedIndex].value;
+        row.cells[2].innerHTML = email.value;
+        row.cells[3].innerHTML = Prix.value;
+        row.cells[4].innerHTML = date.value;
+        row.cells[5].innerHTML = lang.options[lang.selectedIndex].value;
         console.log(lang.options[lang.selectedIndex].value);
         for (var i = 0; i < rad.length; i++) {
             if (rad[i].checked) {
-                row.cells[5] = rad[i].value;
+                row.cells[6].innerHTML = rad[i].value;
             }
         }
         r.innerHTML = "Edit";
         document.getElementById("btn").removeAttribute("disabled")
     }
 }
+function trier(){
+    liste.sort(function(a,b){
+        if (a.titre < b.titre){
+            return -1
+        }
+        if (a.titre > b.titre){
+            return 1
+        }
+        else {
+            return 0
+        }
+    })
+}
+function printTable(){
+    var tableDiv = document.getElementsByTagName("tbody")[0].innerHTML;
+    var bodyContent = document.body.innerHTML;
+
+    document.body.innerHTML = tableDiv;
+    window.print();
+    document.body.innerHTML = bodyContent;
+}
+
